9be3c2945e8f94a069195a5cb87c792b8c12a079
tejaswini <tejaswini076@gmail.com>
2020-11-16 13:54:27 -0500

    Adds changes in Courses class

--- src/main/java/com/api/service/serviceImpl/CourseServiceImpl.java
+++ src/main/java/com/api/service/serviceImpl/CourseServiceImpl.java
@@ -3,11 +3,12 @@ package com.api.service.serviceImpl;
 import com.api.entity.Course;
 import com.api.repositories.CourseRepository;
 import com.api.service.CourseService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
 
+@Slf4j
 @Service
 public class CourseServiceImpl implements CourseService {
 
@@ -24,6 +25,7 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public List<Course> getCourses() {
+        log.info("fetching courses");
         List<Course> courses = new ArrayList<>();
        Iterable<Course> iterator= elasticsearchRepository.findAll();
        iterator.forEach(c->{
